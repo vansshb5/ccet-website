f76e62b439daea3edad33c7bb2177213d64e74b1
ArnavJoshi75 <arnavjoshi2035@gmail.com>
2025-06-22 22:10:55 +0530

    HomePage banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,19 @@ import EventCalendar from './EventCalendar'
 function Home() {
   return (
     <div>
+      <div>
+        <style>
+          {`
+            .home-banner {
+              width: 100vw;
+              max-width: 100vw;
+              margin-left: calc(-50vw + 50%);
+              display: block;
+            }
+          `}
+        </style>
+        <img className="home-banner" src="src/assets/home/banner.png" alt="Banner" />
+      </div>
       <div className="flex flex-col md:flex-row gap-4 justify-center">
         <LatestEvents />
         <div className="flex justify-center items-center">
