25091ccfac14876e2fd2d96c2c277d7044d7e588
SahilKumar-064738 <sahilkumar064738@gmail.com>
2025-07-04 18:08:10 +0530

    setup canvas for AcademicProspectusPage layout

---
+++ src/pages/Academic-Prospectus/AcademicProspectusPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AcademicProspectusPage.css';
+
+const AcademicProspectusPage = () => {
+  return (
+    <div className="academic-prospectus-container">
+      {/* Content will go here */}
+    </div>
+  );
+};
+
+export default AcademicProspectusPage;
