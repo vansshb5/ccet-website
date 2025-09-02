583059cc811e1370dca1e1cf62dde1f5a16375a0
Sahil Garg <me.sahil.gg@gmail.com>
2025-09-02 21:01:12 +0530

    fix: fixed some double imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./index.css";
 import NoPage from "./pages/Nopage/NoPage.jsx";
 import FAQ from "./pages/FAQ/faq";
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home.jsx';
-import Header from './components/header/Header.jsx';
-import Footer from './components/Footer/Footer.jsx';
-
 import VisionMission from "./pages/vision-mission/vision-mission.jsx";
 import Convocation from "./pages/Convocation/Convocation.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./index.css";
-import NoPage from "./pages/Nopage/NoPage.jsx";
-import FAQ from "./pages/FAQ/faq";
 import AntiRagging from "./pages/Students-Section/Student-Welfare/Anti-Ragging/AntiRagging.jsx";
 import AntiRaggingCommittee from "./pages/Students-Section/Student-Welfare/Anti-Ragging-Committee/AntiRaggingCommittee";
 import Academicheads from "./pages/AcadmicHeads/Academicheads.jsx";
@@ -30,11 +21,8 @@ import AcademicProspectusPage from "./pages/Academic-Prospectus/AcademicProspect
 import ContactUs from "./pages/ContactUs/ContactUs.jsx";
 import NIRF from "./pages/NIRF/nirf.jsx";
 import HelpDesk from "./pages/HelpDesk/HelpDesk.jsx";
-
 import Examinations from "./pages/Examinations/Examinations.jsx";
-
 import ECell from "./pages/ECell/ECell.jsx";
-
 import SportsFacility from "./pages/Sports/SportsFacility.jsx";
 import FitIndiaPage from "./pages/Students-Section/Academics/FitIndia/FitIndiaPage.jsx";
 import Leet from "./pages/Degree-PU-LEET/Degree-pu-leet.jsx";
@@ -43,62 +31,67 @@ import Degree from "./pages/Degree/Degree.jsx";
 import Tenders from "./pages/Tenders/Tenders.jsx";
 
 function App() {
-  return (
-    <HashRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NoPage />} />
-        <Route path="/faq" element={<FAQ />} />
+	return (
+		<HashRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<NoPage />} />
+				<Route path="/faq" element={<FAQ />} />
 
-        <Route path="*" element={<NoPage />} />
+				<Route path="*" element={<NoPage />} />
 
-        <Route path="/anti-ragging" element={<AntiRagging />} />
-        <Route
-          path="/anti-ragging-committee"
-          element={<AntiRaggingCommittee />}
-        />
-        <Route path="/about/academic-heads" element={<Academicheads />} />
-        <Route path="/courses" element={<Courses />} />
+				<Route path="/anti-ragging" element={<AntiRagging />} />
+				<Route
+					path="/anti-ragging-committee"
+					element={<AntiRaggingCommittee />}
+				/>
+				<Route
+					path="/about/academic-heads"
+					element={<Academicheads />}
+				/>
+				<Route path="/courses" element={<Courses />} />
 
-        <Route path="/about/principal" element={<Principal />} />
+				<Route path="/about/principal" element={<Principal />} />
 
-        <Route path="/vision-mission" element={<VisionMission />} />
-        <Route path="/Convocation" element={<Convocation />} />
-        <Route path="/about/principal" element={<Principal />} />
-        <Route path="/students-section/courses" element={<Courses />} />
-        <Route path="/prospectus" element={<AcademicProspectusPage />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/academics/nirf" element={<NIRF />} />
+				<Route path="/vision-mission" element={<VisionMission />} />
+				<Route path="/Convocation" element={<Convocation />} />
+				<Route path="/about/principal" element={<Principal />} />
+				<Route path="/students-section/courses" element={<Courses />} />
+				<Route
+					path="/prospectus"
+					element={<AcademicProspectusPage />}
+				/>
+				<Route path="/contact" element={<ContactUs />} />
+				<Route path="/academics/nirf" element={<NIRF />} />
 
+				<Route path="/helpdesk" element={<HelpDesk />} />
 
-        <Route path="/helpdesk" element={<HelpDesk />} />
+				<Route path="/ecell" element={<ECell />} />
+				<Route path="/examinations" element={<Examinations />} />
 
-        <Route path='/ecell' element={<ECell />} />
-        <Route path='/examinations' element={<Examinations />} />
+				<Route path="/ecell" element={<ECell />} />
+				<Route path="/examinations" element={<Examinations />} />
+				<Route path="/sports-facility" element={<SportsFacility />} />
+				<Route path="/fit-india" element={<FitIndiaPage />} />
 
-        <Route path='/ecell' element={<ECell />} />
-        <Route path='/examinations' element={<Examinations />} />
-        <Route path='/sports-facility' element={<SportsFacility />} />
-         <Route path="/fit-india" element={<FitIndiaPage />} />
+				<Route path="/ecell" element={<ECell />} />
+				<Route path="/examinations" element={<Examinations />} />
 
-        <Route path="/ecell" element={<ECell />} />
-        <Route path="/examinations" element={<Examinations />} />
+				<Route path="/ecell" element={<ECell />} />
+				<Route path="/examinations" element={<Examinations />} />
+				<Route path="/sports-facility" element={<SportsFacility />} />
+				<Route path="/fit-india" element={<FitIndiaPage />} />
+				<Route path="/programmes/pu-leet" element={<Leet />} />
+				<Route path="/programmes/phd" element={<Doctorate />} />
+				<Route path="/programmes/degree" element={<Degree />} />
+				<Route path="/notices/tenders" element={<Tenders />} />
 
-        <Route path="/ecell" element={<ECell />} />
-        <Route path="/examinations" element={<Examinations />} />
-        <Route path="/sports-facility" element={<SportsFacility />} />
-        <Route path="/fit-india" element={<FitIndiaPage />} />
-        <Route path="/programmes/pu-leet" element={<Leet />} />
-        <Route path="/programmes/phd" element={<Doctorate />} />
-        <Route path="/programmes/degree" element={<Degree />} />
-        <Route path="/notices/tenders" element={<Tenders />} />
-
-        {/* Add more routes here */}
-      </Routes>
-      <Footer />
-    </HashRouter>
-  );
+				{/* Add more routes here */}
+			</Routes>
+			<Footer />
+		</HashRouter>
+	);
 }
 
 export default App;
