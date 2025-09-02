236db2ba4a0f07235b1bb39a28fbb54ff8f0c11e
SAHIL KUMAR <sahilkumar064738@gmail.com>
2025-09-02 19:13:26 +0530

    link added (#102)

--- src/components/header/AdmissionsMenu.jsx
+++ src/components/header/AdmissionsMenu.jsx
@@ -6,6 +6,39 @@ const AdmissionsMenu = () => {
   const sections = [
     {
       title: "Admissions",
+      links: ["Admission Notices", "Help Desk"],
+    },
+    {
+      title: "Programmes",
+      links: ["Degree Course", "Degree Course (PU-LEET)", "Doctorate (PhD)"],
+
+    },
+    {
+      title: "JAC",
+      links: [
+        { name: "Portal", url: "https://jacchd.admissions.nic.in/" },
+        {
+          name: "Counselling Schedule",
+          url: "https://jacchd.admissions.nic.in/schedule/",
+        },
+        "JAC Brochure 2025",
+        "Opening/Closing Rank",
+      ],
+    },
+    {
+      title: "Criteria",
+      links: [
+        {
+          name: "Eligibility",
+          url: "https://jacchd.admissions.nic.in/eligibility-status/",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="absolute top-full left-1/2 transform -translate-x-[65%] hidden group-hover:grid grid-cols-4 bg-white/70 backdrop-blur-md shadow-xl z-50 p-6 gap-6 text-1xl text-gray-800 rounded-lg border border-gray-100 min-w-[1000px] max-w-[90vw]">
+
       links: [
         { name: "Admission Notices", path: "/admissions/notices" },
         { name: "Help Desk", path: "/admissions/helpdesk" },
@@ -103,12 +136,20 @@ const AdmissionsMenu = () => {
                 className="hover:bg-[#FB923C] hover:text-white cursor-pointer transition-colors duration-200 px-2 py-1 rounded"
               >
                 {typeof link === "string" ? (
+                  link
+                ) : link.url.startsWith("http") ? (
+
+                {link.url ? (
+
+
                   <span>{link}</span>
                 ) : link.url ? (
                   <a href={link.url} target="_blank" rel="noopener noreferrer">
                     {link.name}
                   </a>
                 ) : (
+
+                  <a href={link.url}>{link.name}</a>
                   <Link to={link.path}>{link.name}</Link>
                 )}
               </li>
