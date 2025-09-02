8598f8487d2641597481c052ec4c7fd5be35c901
SAHIL KUMAR <sahilkumar064738@gmail.com>
2025-09-02 15:32:05 +0530

    Add CCET | Opening/Closing Rank (#100)
    * link added
    
    * Add CCET | Opening/Closing Rank

--- src/components/header/AdmissionsMenu.jsx
+++ src/components/header/AdmissionsMenu.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
+
 
 const AdmissionsMenu = () => {
   const sections = [
     {
       title: "Admissions",
+
+      links: ["Admission Notices", "Help Desk"],
+    },
+    {
+      title: "Programmes",
+      links: ["Degree Course", "Degree Course (PU-LEET)", "Doctorate (PhD)"],
+
       links: [
         { name: "Admission Notices", path: "/admissions/notices" },
         { name: "Help Desk", path: "/admissions/helpdesk" },
@@ -17,6 +26,7 @@ const AdmissionsMenu = () => {
         { name: "Degree Course (PU-LEET)", path: "/programmes/pu-leet" },
         { name: "Doctorate (PhD)", path: "/programmes/phd" },
       ],
+
     },
     {
       title: "JAC",
@@ -30,17 +40,32 @@ const AdmissionsMenu = () => {
           name: "JAC Brochure 2025",
           url: "https://jacchd.admissions.nic.in/information-bulletin/",
         },
+
+        {
+          name: "Opening/Closing Rank",
+          url: "https://jacchd.admissions.nic.in/or-cr/",
+        },
+
         "Opening/Closing Rank",
+
       ],
     },
     {
       title: "Criteria",
+
+      links: ["Eligibility"],
+
       links: [{ name: "Eligibility", path: "/criteria/eligibility" }],
+
     },
   ];
 
   return (
+
+    <div className="absolute top-full left-1/2 transform -translate-x-[65%] hidden group-hover:grid grid-cols-4 bg-white/70 backdrop-blur-md shadow-xl z-50 p-6 gap-6 text-1xl text-gray-800 rounded-lg border border-gray-100 min-w-[1000px] max-w-[90vw]">
+
     <div className="absolute top-full left-1/2 transform -translate-x-[65%] hidden group-hover:grid grid-cols-4 bg-white/70 backdrop-blur-md shadow-xl z-50 p-6 gap-6 text-base text-gray-800 rounded-lg border border-gray-100 min-w-[1000px] max-w-[90vw]">
+
       {sections.map((section, i) => (
         <div key={i}>
           <div className="font-semibold border-b border-gray-200 pb-2 mb-3 text-red-700">
@@ -52,12 +77,22 @@ const AdmissionsMenu = () => {
                 key={idx}
                 className="hover:bg-[#FB923C] hover:text-white cursor-pointer transition-colors duration-200 px-2 py-1 rounded"
               >
+
+                {typeof link === "string" ? (
+                  link
+                ) : link.url.startsWith("http") ? (
+
                 {link.url ? (
+
                   <a href={link.url} target="_blank" rel="noopener noreferrer">
                     {link.name}
                   </a>
                 ) : (
+
+                  <a href={link.url}>{link.name}</a>
+
                   <Link to={link.path}>{link.name}</Link>
+
                 )}
               </li>
             ))}
